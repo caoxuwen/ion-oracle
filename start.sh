abbd087ebd1fd23920fdabcc71ac7057310c289c
Xuwen <xuwen@ion.one>
2019-01-05 22:00:51 +0800

    start script

--- start.sh
+++ start.sh
@@ -3,3 +3,9 @@ sleep 45
 
 source /home/ubuntu/.bashrc
 /usr/bin/forever start /home/ubuntu/ion-oracle/forever/config.json
+
+sleep 5
+/usr/bin/forever start /home/ubuntu/ion-oracle/forever/config-liquidation.json
+
+sleep 5
+/usr/bin/forever start /home/ubuntu/ion-oracle/forever/config-funding.json
